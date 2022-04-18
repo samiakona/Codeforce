889b03a6b0362ba16efe51f2cb66995c3fd9b48d
Samia_Kona <69978344+samiakona@users.noreply.github.com>
2022-04-18 23:01:02 +0600

    Add files via upload

---
+++ A. Bear and Big Brother.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a, b, cnt=0;
+    cin >> a >> b;
+    while (a<=b)
+    {
+        a=a*3;
+        b=b*2;
+        cnt++;
+    }
+    cout << cnt << endl;
+    return 0;
+}
