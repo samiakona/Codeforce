979cf13e0727b32a4e321074e7d2cc442690e4dc
Samia_Kona <69978344+samiakona@users.noreply.github.com>
2022-04-21 13:01:16 +0600

    Add files via upload

---
+++ A. Way Too Long Words.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    int i,n;
+    cin >> n;
+    for(i=0; i<n; i++)
+    {
+        cin >> s;
+
+        if (s.length()>10)
+        {
+            cout << s[0] << s.length()-2 << s[s.length()-1] << endl;
+        }
+        else
+        {
+            cout << s << endl;
+        }
+    }
+    return 0;
+}
